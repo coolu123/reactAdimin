711710001939fb28a7446148e04ab147c1010dc4
Your Name <you@example.com>
2019-12-12 11:12:40 +0800

    admin-app

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react';
+
+class App extends Component{
+    render(){
+      return (
+          <div className="App" style={{textAlign:'center'}}>
+              <h1>App主组件</h1>
+          </div>
+      );
+  }
+}
+
+export default App;
